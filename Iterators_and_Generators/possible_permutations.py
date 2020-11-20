b39067a388cf4bfdd6c76af7974ad44637d00ba3
Vikadie <68245263+Vikadie@users.noreply.github.com>
2020-11-20 22:25:09 +0200

    Python OOP Iterators and Generators

--- Iterators_and_Generators/possible_permutations.py
+++ Iterators_and_Generators/possible_permutations.py
@@ -1,9 +1,28 @@
-from itertools import permutations
+# from itertools import permutations
+#
+#
+# def possible_permutations(lst):
+#     for permutation in permutations(lst):
+#         yield list(permutation)
+#
+#
+# [print(n) for n in possible_permutations([1, 2, 3])]
 
 
-def possible_permutations(lst):
-    for permutation in permutations(lst):
-        yield list(permutation)
+perm = [0, 0, 0]
+used = 3 * [False]
 
 
-[print(n) for n in possible_permutations([1, 2, 3])]
+def pos_perm(seq, idx = 0):
+    if idx == len(seq):
+        print(perm)
+        return
+
+    for i, x in enumerate(seq):
+        if not used[i]:
+            perm[idx] = x
+            used[i] = True
+            pos_perm(seq, idx + 1)
+            used[i] = False
+
+print(pos_perm([1, 2, 3]))
